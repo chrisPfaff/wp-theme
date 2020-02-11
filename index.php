334d1f70ac8e05bb6040f174c7973983138e2c37
Chris Pfaff <chrispfaff10@gmail.com>
2020-02-10 21:51:24 -0600

    added php loop for link and content

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="./style.css">
 </head>
 <body>
   <h1><?php blogInfo('name');?></h1>
-  <p>Students</p>
+  <!-- <p>Students</p>
   <ul>
   <?php
   //! wordpress uses while loops;
@@ -11,12 +19,14 @@
     echo "<li> My name is $students[$count] </li>";
     $count++;
   }
-  ?>
+  ?> -->
   </ul>
   <?php
     while(have_posts()) {
       the_post(); ?>
-      <h2><?php the_title(); ?></h2>
+      <h2> <a href="<?php the_permalink();?>"> <?php the_title(); ?> </a> </h2>
+      <?php the_content(); ?>
+      <hr>
     <?php }
   ?>
 </body>
